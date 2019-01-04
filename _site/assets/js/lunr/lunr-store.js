9d05f8dff0977927f5238dab9667f71d39cfa74c
Casey Hagen <aechagen@OASIS-Surface.localdomain>
2019-01-05 08:59:38 +0900

    Trying to fix gh-pages

---
+++ _site/assets/js/lunr/lunr-store.js
@@ -0,0 +1,11 @@
+var store = [{
+        "title": "Test",
+        "excerpt":"This is a test   To see if it works   Maybe it does, maybe it doesn’t.  ","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/test/",
+        "teaser":null},{
+        "title": "Welcome to Jekyll!",
+        "excerpt":"You’ll find this post in your _posts directory. Go ahead and edit it and re-build the site to see your changes. You can rebuild the site in many different ways, but the most common way is to run jekyll serve, which launches a web server and auto-regenerates your site when...","categories": ["jekyll","update"],
+        "tags": [],
+        "url": "http://localhost:4000/jekyll/update/welcome-to-jekyll/",
+        "teaser":null}]
